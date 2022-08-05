0e000120411ad34f09ab78fda3b5be210a032c16
CassieV <obicassievirtue@gmail.com>
2022-08-05 15:07:26 +0200

    Add module to create random number.. to generate numbers individually between 1 and 6 only

--- mastermind.rb
+++ mastermind.rb
@@ -1,16 +1,42 @@
 # frozen_string_literal: true
 
-class Mastermind
+module Master
+  def random_num
+    num = (1..6).to_a
+    arr = []
+    4.times do
+      arr.push(num.sample)
+    end
+    rand_num = arr.join().to_i
+  end
+end
 
+
+class Mastermind
+  include Master
   def initialize
-    @list = (1111..6666).to_a
+    @num
   end
 
   def random_number
-    # list = (1111..6666).to_a
-    puts @list.sample
+    @num = random_num
   end
+
+  def guess_number
+    puts @num
+    2.times do
+      player = gets.chomp.to_i
+      if player == @num
+      puts 'You win'
+      break
+      else
+        puts 'Try again..'
+      end
+    end
+  end
+
 end
 
 mastermind = Mastermind.new
 mastermind.random_number
+mastermind.guess_number
