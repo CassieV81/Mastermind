182cf3a072f41fcb72d32ba96bcc8b73f302d6bc
CassieV <obicassievirtue@gmail.com>
2022-08-05 16:07:13 +0200

    Add method for checking correct guess

--- mastermind.rb
+++ mastermind.rb
@@ -3,30 +3,37 @@
 module Master
   def random_num
     num = (1..6).to_a
-    rand_num = num.sample(4)
+    num.sample(4)
   end
 end
 
 
 class Mastermind
+
   include Master
   def initialize
-    @num
-  end
-
-  def random_number
     @num = random_num
+    @player
   end
 
   def guess_number
-    puts @num
+    p @num
     2.times do
-      player = gets.chomp.to_i
-      if player == @num
-      puts 'You win'
-      break
+      @player = gets.chomp.to_i.digits.reverse
+      if @player == @num
+        puts 'You win'
+        break
       else
+        check_num
         puts 'Try again..'
+      end
+    end
+  end
+
+  def check_num
+    @num.any? do |i|
+      if @player.include?(i)
+        puts 'o'
       end
     end
   end
@@ -34,5 +41,5 @@ class Mastermind
 end
 
 mastermind = Mastermind.new
-mastermind.random_number
+# mastermind.random_number
 mastermind.guess_number
